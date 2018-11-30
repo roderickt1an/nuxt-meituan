c6325f08df3fb52ff73b2e336e6b8a86c6d2b178
roderickt1an <roderickt1an@gmail.com>
2018-11-30 18:05:29 +0800

    regesitr

--- pages/register.vue
+++ pages/register.vue
@@ -83,7 +83,27 @@ export default {
         cpwd: ""
       },
       rules: {
-
+        name: [{
+          required: true, type: "string", message: "请输入昵称", trigger: 'blur'
+        }],
+        email: [
+          { required: true, type: "email", message: "请输入邮箱", trigger: 'blur' }
+        ],
+        pwd: [
+          { required: true, message: "创建密码", trigger: 'blur'}
+        ],
+        cpwd: [
+          { required: true, message: "确认密码", trigger: 'blur' },
+          { validator: (rule, value, callback) => {
+            if(value === ''){
+              callback(new Error('请再次输入密码！'))
+            }else if(value != this.ruleForm.pwd){
+              callback(new Error('两次输入密码不一致！'))
+            }else{
+              callback()
+            }
+          }, trigger: 'blur'}
+        ]
       },
       statusMsg: "",
       error: ""
